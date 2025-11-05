e9f8aee89aa698ec3e06559b46e06caa624219a9
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-11-05 21:04:28 +0100

    Servo controll and main file added

--- Robot_arm/main.py
+++ Robot_arm/main.py
@@ -1,18 +1,18 @@
-from lib.servo_control import set_servo_angle, create_servo
+from Robot_arm.servo_control import set_servo_angle, create_servo
+from time import sleep
 
 #--Servo setup--
 base = create_servo(1, 50, 1150, 8600)
 # joint1 = create_servo(2, 50, 1150, 8600)
 # joint2 = create_servo(3, 50, 1150, 8600)
 # joint3 = create_servo(1, 50, 1150, 8600)  #wrist roll
-# joint4 = create_servo(1, 50, 1150, 8600)  #wrist up/down
-# gripper = create_servo(1, 50, 1150, 8600)
+# joint4 = create_servo(1, 50, 1150, 8600
 
-
-
-
-
-#Calibrate
 
 print("Servo to angle 0:")
 set_servo_angle(base, 0)
+sleep(4)
+print("Servo to angle 180:")
+set_servo_angle(base, 180)
+sleep(4)
+print("Done")
