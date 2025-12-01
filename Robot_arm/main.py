051b266f75022da50cb1ef1e723ede833fa56d17
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-12-01 15:01:12 +0100

    added UDP connection to fix Pico timeout bug

--- Robot_arm/main.py
+++ Robot_arm/main.py
@@ -2,12 +2,14 @@ from Robot_arm.servo_control import set_servo_angle, create_servo
 from time import sleep
 import network
 import socket
+import select
 
 #--Servo setup--
 base = create_servo(1, 50, 1150, 8600)
-# joint1 = create_servo(2, 50, 1150, 8600)
-# joint2 = create_servo(3, 50, 1150, 8600)
-# joint3 = create_servo(1, 50, 1150, 8600)  #wrist roll
+shoulder = create_servo(2, 50, 1150, 8600)
+elbow = create_servo(3, 50, 1150, 8600)
+wrist_joint = create_servo(4, 50, 1150, 7840)  #wrist roll
+gripper = create_servo(5, 50, 1150, 7840)
 # joint4 = create_servo(1, 50, 1150, 8600
 
 #--Wifi--
@@ -28,17 +30,17 @@ def connect():
 def parse_request(request_str):
     angles = {}
     
-    # Find the query part of the URL
-    query_start = request_str.find('?')
-    if query_start == -1:
-        return angles # No '?' found
+    # # Find the query part of the URL
+    # query_start = request_str.find('?')
+    # if query_start == -1:
+    #     return angles # No '?' found
         
-    query_str = request_str[query_start+1:]
-    #Removes 'HTTP/1.1' from query str
-    clean_query_str = query_str.split(' ')[0]
+    # query_str = request_str[query_start+1:]
+    # #Removes 'HTTP/1.1' from query str
+    # clean_query_str = query_str.split(' ')[0]
 
     # Split by '&' to get "j0=90", "j1=45", etc.
-    commands = clean_query_str.split('&')
+    commands = request_str.split('&')
     for cmd in commands:
         try:
             # Split "j0=90" into "j0" and "90"
@@ -77,21 +79,19 @@ def web_control():
                 print("RECIVED VALUES: ", angles_to_set)
 
             # --Activate Servos--
-            # if angles_to_set:
-            #     print("Received angles:", angles_to_set)
-
-            #     if 0 in angles_to_set:
-            #         set_servo_angle(base, angles_to_set[0])
-            #     if 1 in angles_to_set:
-            #         set_servo_angle(base, angles_to_set[0])
-            #     if 2 in angles_to_set:
-            #         set_servo_angle(base, angles_to_set[0])
+            if angles_to_set:
+                if 0 in angles_to_set:
+                    set_servo_angle(base, angles_to_set[0])
+                if 1 in angles_to_set:
+                    set_servo_angle(shoulder, angles_to_set[1])
+                if 2 in angles_to_set:
+                    set_servo_angle(elbow, angles_to_set[2])
             #     if 3 in angles_to_set:
             #         set_servo_angle(base, angles_to_set[0])
             #     if 4 in angles_to_set:
             #         set_servo_angle(base, angles_to_set[0])
-            #     if 6 in angles_to_set:
-            #         set_servo_angle(base, angles_to_set[0])
+                if 5 in angles_to_set:
+                    set_servo_angle(gripper, angles_to_set[5])
 
             # Send a simple "OK" response back to the PC
             conn.send('HTTP/1.0 200 OK\r\nContent-type: text/plain\r\n\r\nOK')
@@ -103,5 +103,49 @@ def web_control():
                 conn.close()
             print("Connection closed due to error")
 
-        
-web_control()
+def udp_control():
+    connect()
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    try:
+        s.bind(('0.0.0.0', 5000))
+    except OSError:
+        print("Port busy, resetting...")
+        return
+    print("Listening for UDP commands on Port 5000...")
+
+    poller = select.poll()
+    poller.register(s, select.POLLIN)
+
+    while True:
+        try:
+            res = poller.poll(100)
+            
+            if res:
+                data, addr = s.recvfrom(1024)
+                message = data.decode('utf-8')
+                angles_to_set = parse_request(message)
+
+                # --Activate Servos--
+                if angles_to_set:
+                    print("RECIVED VALUES: ", angles_to_set)
+                    if 0 in angles_to_set:
+                        set_servo_angle(base, angles_to_set[0])
+                    if 1 in angles_to_set:
+                        set_servo_angle(shoulder, angles_to_set[1])
+                    if 2 in angles_to_set:
+                        set_servo_angle(elbow, angles_to_set[2])
+                #     if 3 in angles_to_set:
+                #         set_servo_angle(base, angles_to_set[0])
+                #     if 4 in angles_to_set:
+                #         set_servo_angle(base, angles_to_set[0])
+                    if 5 in angles_to_set:
+                        set_servo_angle(gripper, angles_to_set[5])
+
+        except Exception as e:
+            print("Error:", e)
+
+
+udp_control()
+
+#test wrist
+#set_servo_angle(wrist_joint, 0)
