7eec896bfaf9c03954ca6592d9c8839e7e5c36ab
Stefan1711UNI <stefan.sonderling1@student.nhlstenden.com>
2025-12-14 14:10:40 +0100

    Added wrist controll

--- Robot_arm/main.py
+++ Robot_arm/main.py
@@ -8,9 +8,9 @@ import select
 base = create_servo(1, 50, 1150, 8600)
 shoulder = create_servo(2, 50, 1150, 8600)
 elbow = create_servo(3, 50, 1150, 8600)
-wrist_joint = create_servo(4, 50, 1150, 7840)  #wrist roll
+wrist_joint = create_servo(4, 50, 1150, 7840)  #wrist up/down
 gripper = create_servo(5, 50, 1150, 7840)
-# joint4 = create_servo(1, 50, 1150, 8600
+#joint4 = create_servo(1, 50, 1150, 7840)
 
 #--Wifi--
 def connect():
@@ -103,6 +103,8 @@ def web_control():
                 conn.close()
             print("Connection closed due to error")
 
+
+
 def udp_control():
     connect()
     s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
@@ -134,12 +136,15 @@ def udp_control():
                         set_servo_angle(shoulder, angles_to_set[1])
                     if 2 in angles_to_set:
                         set_servo_angle(elbow, angles_to_set[2])
-                #     if 3 in angles_to_set:
-                #         set_servo_angle(base, angles_to_set[0])
+                    if 3 in angles_to_set:
+                        set_servo_angle(wrist_joint, angles_to_set[3])
                 #     if 4 in angles_to_set:
                 #         set_servo_angle(base, angles_to_set[0])
                     if 5 in angles_to_set:
                         set_servo_angle(gripper, angles_to_set[5])
+
+                #constant
+                #set_servo_angle(wrist_joint, 90)
 
         except Exception as e:
             print("Error:", e)
@@ -148,4 +153,8 @@ def udp_control():
 udp_control()
 
 #test wrist
-#set_servo_angle(wrist_joint, 0)
+# for i in range(20):
+#     print(i)
+#     a = 90 + 60
+#     set_servo_angle(shoulder, a)
+#     sleep(0.3)
